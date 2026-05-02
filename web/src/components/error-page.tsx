6849819795237bc91a2dfa03f29f093acb31b791
ossaidqadri <imossaidquadri@gmail.com>
2026-05-02 17:55:41 +0500

    refactor: replace img elements with next/image

--- web/src/components/error-page.tsx
+++ web/src/components/error-page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 const ErrorPage = () => {
   return (
@@ -17,11 +18,12 @@ const ErrorPage = () => {
       {/* Right Section: Illustration */}
       <div className="relative flex h-[400px] max-h-screen w-full p-2 lg:h-full">
         <div className="h-full w-full rounded-2xl bg-black" />
-        {/* biome-ignore lint/performance/noImgElement: External CDN image for error illustration */}
-        <img
+        <Image
           src="https://cdn.shadcnstudio.com/ss-assets/blocks/marketing/error/image-1.png"
           alt="404 illustration"
-          className="absolute top-1/2 left-1/2 h-[clamp(200px,40vw,406px)] -translate-x-1/2 -translate-y-1/2 lg:h-[clamp(260px,25vw,406px)]"
+          fill
+          className="object-cover absolute top-1/2 left-1/2 h-[clamp(200px,40vw,406px)] -translate-x-1/2 -translate-y-1/2 lg:h-[clamp(260px,25vw,406px)]"
+          unoptimized
         />
       </div>
     </div>
